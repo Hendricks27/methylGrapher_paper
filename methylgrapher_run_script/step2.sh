df03bc3ff08a2db7172d126824b7aa60a978461b
Hendricks27 <wenjinzhang95@gmail.com>
2024-03-12 23:44:24 -0500

    scripts used for the paper

---
+++ methylgrapher_run_script/step2.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+
+mkdir -p HG00621/fastqc/raw/BRep1.R1
+fastqc -o HG00621/fastqc/raw/BRep1.R1 -t 10 ./HG00621/BRep1.R1.fastq.gz
+
+mkdir -p HG00621/fastqc/raw/BRep1.R2
+fastqc -o HG00621/fastqc/raw/BRep1.R2 -t 10 ./HG00621/BRep1.R2.fastq.gz
+
+mkdir -p HG00621/fastqc/raw/BRep2.R1
+fastqc -o HG00621/fastqc/raw/BRep2.R1 -t 10 ./HG00621/BRep2.R1.fastq.gz
+
+mkdir -p HG00621/fastqc/raw/BRep2.R2
+fastqc -o HG00621/fastqc/raw/BRep2.R2 -t 10 ./HG00621/BRep2.R2.fastq.gz
+
+mkdir -p HG00741/fastqc/raw/BRep1.R1
+fastqc -o HG00741/fastqc/raw/BRep1.R1 -t 10 ./HG00741/BRep1.R1.fastq.gz
+
+mkdir -p HG00741/fastqc/raw/BRep1.R2
+fastqc -o HG00741/fastqc/raw/BRep1.R2 -t 10 ./HG00741/BRep1.R2.fastq.gz
+
+mkdir -p HG00741/fastqc/raw/BRep2.R1
+fastqc -o HG00741/fastqc/raw/BRep2.R1 -t 10 ./HG00741/BRep2.R1.fastq.gz
+
+mkdir -p HG00741/fastqc/raw/BRep2.R2
+fastqc -o HG00741/fastqc/raw/BRep2.R2 -t 10 ./HG00741/BRep2.R2.fastq.gz
+
+mkdir -p HG01952/fastqc/raw/BRep1.R1
+fastqc -o HG01952/fastqc/raw/BRep1.R1 -t 10 ./HG01952/BRep1.R1.fastq.gz
+
+mkdir -p HG01952/fastqc/raw/BRep1.R2
+fastqc -o HG01952/fastqc/raw/BRep1.R2 -t 10 ./HG01952/BRep1.R2.fastq.gz
+
+mkdir -p HG01952/fastqc/raw/BRep2.R1
+fastqc -o HG01952/fastqc/raw/BRep2.R1 -t 10 ./HG01952/BRep2.R1.fastq.gz
+
+mkdir -p HG01952/fastqc/raw/BRep2.R2
+fastqc -o HG01952/fastqc/raw/BRep2.R2 -t 10 ./HG01952/BRep2.R2.fastq.gz
+
+mkdir -p HG01978/fastqc/raw/BRep1.R1
+fastqc -o HG01978/fastqc/raw/BRep1.R1 -t 10 ./HG01978/BRep1.R1.fastq.gz
+
+mkdir -p HG01978/fastqc/raw/BRep1.R2
+fastqc -o HG01978/fastqc/raw/BRep1.R2 -t 10 ./HG01978/BRep1.R2.fastq.gz
+
+mkdir -p HG01978/fastqc/raw/BRep2.R1
+fastqc -o HG01978/fastqc/raw/BRep2.R1 -t 10 ./HG01978/BRep2.R1.fastq.gz
+
+mkdir -p HG01978/fastqc/raw/BRep2.R2
+fastqc -o HG01978/fastqc/raw/BRep2.R2 -t 10 ./HG01978/BRep2.R2.fastq.gz
+
+mkdir -p HG03516//fastqc/raw/BRep1.R1
+fastqc -o HG03516//fastqc/raw/BRep1.R1 -t 10 ./HG03516//BRep1.R1.fastq.gz
+
+mkdir -p HG03516//fastqc/raw/BRep1.R2
+fastqc -o HG03516//fastqc/raw/BRep1.R2 -t 10 ./HG03516//BRep1.R2.fastq.gz
+
+mkdir -p HG03516//fastqc/raw/BRep2.R1
+fastqc -o HG03516//fastqc/raw/BRep2.R1 -t 10 ./HG03516//BRep2.R1.fastq.gz
+
+mkdir -p HG03516//fastqc/raw/BRep2.R2
+fastqc -o HG03516//fastqc/raw/BRep2.R2 -t 10 ./HG03516//BRep2.R2.fastq.gz
+
+
